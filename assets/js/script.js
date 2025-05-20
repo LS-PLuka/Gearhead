b5df5920ec6292ecfd5418a0b69226dad53adb1e
Pedro Luka <pedrolukagt@gmail.com>
2025-05-20 16:41:25 -0300

    Sobre Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
   const dropdownToggle = document.querySelectorAll('[data-dropdown-toggle]');
   dropdownToggle.forEach((toggle) => {
       toggle.addEventListener('click', function() {
           const parent = this.closest('.dropdown-item');
           parent.classList.toggle('active');
-      });
-  });
+      })
+  })
 
   //Feechar o dropdown ao clicar fora
   document.addEventListener('click', function (e) {
@@ -13,10 +13,10 @@ document.addEventListener('DOMContentLoaded', function(){
           const parent = toggle.closest('.dropdown-item');
           if(!parent.contains(e.target)) {
               parent.classList.remove('active');
-          };
-      });
-  });
-});
+          }
+      })
+  })
+})
 
 const searchToggle = document.getElementById('searchToggle');
 const searchOverlay = document.getElementById('searchOverlay');
@@ -26,35 +26,56 @@ searchToggle.addEventListener('click', () => {
   console.log(searchOverlay);
   searchOverlay.style.display = 'flex';
   setTimeout(() => {
-    searchOverlay.querySelector('.search-input').focus();
+      searchOverlay.querySelector('.search-input').focus();
   }, 100);
-});
+})
 
-// Fechar o botão
+//Fechar o botão
 closeSearch.addEventListener('click', () => {
   searchOverlay.style.display = 'none';
-});
+})
 
-// Fechar com o ESC
+//Fechar com o ESC 
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape') {
-    searchOverlay.style.display = 'none';
+      searchOverlay.style.display = 'none';
   }
-});
+})
 
-// Galeria
+//Galeria
 const imagens = document.querySelectorAll('.galeria img');
 const lightbox = document.getElementById('lightbox');
 const lightboxImg = document.getElementById('lightbox-img');
 
 imagens.forEach((img) => {
   img.addEventListener('click', () => {
-    lightboxImg.src = img.src;
-    lightbox.classList.add('show');
-  });
-});
+      lightboxImg.src = img.src;
+      lightbox.classList.add('show');
+  })
+})
 
-// Fechar a lightbox
+//Fechar a lightbox
 lightbox.addEventListener('click', () => {
   lightbox.classList.remove('show');
-});
+})
+
+//Abrir o Login
+const openLoginBtn = document.getElementById('openLogin');
+const containerLogin = document.querySelector('.container-login');
+
+openLoginBtn.addEventListener('click', () => {
+  containerLogin.classList.add('active')
+})
+
+//Fechar ao clicar fora do formulário ou pressionar ESC
+containerLogin.addEventListener('click', (e) => {
+  if(e.target === containerLogin) {
+      containerLoginclassList.remove('active')
+  }
+})
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+      containerLogin.classList.remove('active');
+  }
+})
